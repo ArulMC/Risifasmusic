b31dc857ab880f3c8bd0638f8f5b54da7731d947
RizzyFuzz <rizzowohq@gmail.com>
2024-01-02 18:29:07 +0000

    first commit

---
+++ music-list.js
@@ -0,0 +1,68 @@
+let allMusic = [
+    {
+        name: "Fury (w/ kydsky, bsterthegawd)",
+        artist: "Antikvng",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "lonerism.",
+        artist: "zāran",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "last revenge.",
+        artist: "zāran",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "MADNESZ x JSMN - MOSHPIT!",
+        artist: "JSMN",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "STICK!",
+        artist: "JSMN",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "raja iblis. (w/ scioss.)",
+        artist: "zāran",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "the end.",
+        artist: "zāran",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "fallen angel.",
+        artist: "zāran",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Ascent",
+        artist: "luxary x scoolprod x ghxsted. x zāran x scioss",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Seraphim [w/ zāran & scioss.]",
+        artist: "OICHI",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Doom Ethernal [w/zāran]",
+        artist: "RizzyFuzz",
+        img: "music-11",
+        src: "music-11"
+    },
+];
